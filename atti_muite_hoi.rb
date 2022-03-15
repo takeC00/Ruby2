b53bf040f9df22d3af2e4a589b4b63e70b434626
服部剛士 <take.614@icloud.com>
2022-03-15 16:38:59 +0900

    勝負の結果が繰り返されるバグを修正

--- atti_muite_hoi.rb
+++ atti_muite_hoi.rb
@@ -28,33 +28,43 @@ def attimuite_hoi
   puts "相手の手[#{pc_hand}]"
 
   result = put_pc_hand - put_my_hand
+  
   if result == 0
     @put_result = "DRAW"
   end
+
   if put_pc_hand == 0
+
     if put_my_hand == 1
       @put_result = "LOSE"
     elsif put_my_hand == 2
       @put_result = "WIN"
     end
+
   elsif put_pc_hand == 1
+    
     if put_my_hand == 0
       @put_result = "WIN"
     elsif put_my_hand == 2
       @put_result = "LOSE"
     end
+
   elsif put_pc_hand == 2
+
     if put_my_hand == 0
       @put_result = "LOSE"
     elsif put_my_hand == 1
       @put_result = "WIN"
     end
   end
+
   puts "結果#{@put_result}"
   puts "------------------"
+
   if @put_result == "DRAW"
     puts 'あいこで〜〜'
     attimuite_hoi
+
   elsif @put_result == "WIN" || @put_result == "LOSE"
     puts "あっち向いて〜〜〜ほいっ！"
     puts '方向を選んでください'
@@ -62,6 +72,7 @@ def attimuite_hoi
   
     hoi_my_hand = gets.chomp.to_i
     hoi_pc_hand = rand(4)
+
     if hoi_my_hand == 0
       my_dir = "上"
     elsif hoi_my_hand == 1
@@ -81,33 +92,31 @@ def attimuite_hoi
     elsif hoi_pc_hand == 3
       pc_dir = "左"
     end
+    
+    if @put_result == "WIN"
+      puts "自分は[#{my_dir}]を指した"
+      puts "相手は[#{pc_dir}]を向いた"
+      puts "------------------------------"
+
+      if my_dir == pc_dir
+        puts "プレイヤー勝利"
+        puts "GAME SET"
+      else 
+        attimuite_hoi
+      end
+
+    elsif @put_result == "LOSE"
+      puts "相手は[#{pc_dir}]を指した"
+      puts "自分は[#{my_dir}]を向いた"
+      puts "------------------------------"
+      if my_dir == pc_dir
+        puts "プレイヤー敗北"
+        puts "GAME SET"
+      else 
+        attimuite_hoi
+      end
+    end
   end
-  
-  
-  
-  if @put_result == "WIN"
-    puts "自分は[#{my_dir}]を指した"
-    puts "相手は[#{pc_dir}]を向いた"
-    puts "------------------------------"
-    if my_dir == pc_dir
-      puts "プレイヤー勝利"
-      puts "GAME SET"
-    else 
-      attimuite_hoi
-    end
-  elsif @put_result == "LOSE"
-    puts "相手は[#{pc_dir}]を指した"
-    puts "自分は[#{my_dir}]を向いた"
-    puts "------------------------------"
-    if my_dir == pc_dir
-      puts "プレイヤー敗北"
-      puts "GAME SET"
-    else 
-      attimuite_hoi
-    end
-  end  
 end
 
-
 attimuite_hoi
-
